8e4e2dfd4f54ed193b09eaa82aee54f706f58844
ChengGong0602 <rjs0602@163.com>
2022-09-18 07:39:44 +0800

    add presale and public sale

--- contracts/ICO.sol
+++ contracts/ICO.sol
@@ -9,16 +9,30 @@ contract ICO is Ownable{
 
     address public adminWallet;
     // address public icoContract;
-    mapping (address => uint256) userdata;
+    mapping (address => uint256) public userdata;
 
     event tokenTransfer(address _userWallet, uint256 amount);
+    uint256 public presaleMaxAmount = 10000000; // 10 milion
+    uint256 public publicMaxAmount = 150000000; // 150 milion
+    uint256 public presaleAmount = 0;
+    uint256 public publicSaleAmount = 0;
+    uint public saleStatus = 0; // sale status: 0- presale, 1- public sale, 2- other sale
 
     constructor(address _adminWallet)  {
         adminWallet = _adminWallet;             
     }
 
+    //make the ICO website for 10 million presale and 150 million public sale and we will keep the rest in reserve to sell latter
     function transferToken(address _tokenContract, uint256 amount, address _userWallet) public {
         require(msg.sender == adminWallet, "You aren't the adminWallet");
+        if (saleStatus == 0){
+            presaleAmount = presaleAmount+amount;
+            require(presaleAmount < presaleMaxAmount, "Presale is finished");
+        }        
+        else if(saleStatus == 1){
+            publicSaleAmount = publicSaleAmount + amount;
+            require(publicSaleAmount < publicMaxAmount, "Public Sale is finished");
+        }
         userdata[_userWallet] = userdata[_userWallet] + amount;
         uint256 depositAmount = userdata[_userWallet];
         IERC20(_tokenContract).transferFrom(adminWallet, _userWallet, amount);
@@ -27,6 +41,11 @@ contract ICO is Ownable{
 
     function updateAdmin(address _newAddress) external onlyOwner{
         adminWallet = _newAddress;
+    }
+
+    // sale status: 0- presale, 1- public sale, 2- other sale
+    function updateSaleStatus(uint _value) external onlyOwner{        
+        saleStatus = _value;
     }
 
 }
